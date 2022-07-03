da4b5c0a8e0cb015a48d90193372bcdfbc581421
tnfo222 <github.com/tnfo222/psychic-disco>
2022-07-02 20:59:50 -0400

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Route Homepage
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+//Route 404
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+app.listen(process.env.PORT)
