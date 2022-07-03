1a10deb803fd0493470efc84e8e649aa18c60569
tnfo222 <github.com/tnfo222/psychic-disco>
2022-07-03 11:47:29 -0400

    Adding 404 page view

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended:true}))
 
+//Controllers and Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-  res.send('Hello world home page')
+  res.render('Home')
 })
 
 app.get('*', (req, res) => {
   res.render('error404')
 })
 
+//Connectivity
 app.listen(process.env.PORT)
 
