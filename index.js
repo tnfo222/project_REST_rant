8529e2bd0ad01266e12f364b6b0c08a0caca87aa
tnfo222 <github.com/tnfo222/psychic-disco>
2022-07-02 21:05:34 -0400

    Adding places controller

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Route for places
+app.use('/places', require('./controllers/places'))
+
 //Route Homepage
 app.get('/', (req, res) => {
     res.send('Hello world!')
