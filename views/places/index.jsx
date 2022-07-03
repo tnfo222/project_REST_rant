8018e15f211294eeb986e5f3ad32031f0fc2b8f6
tnfo222 <github.com/tnfo222/psychic-disco>
2022-07-03 13:26:21 -0400

    Adding places view with mock data

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,11 +1,20 @@
 const React = require('react')
 const Def = require('../default')
 
-function index () {
+function index (data) {
+    let placesFormatted = data.places.map((place) => {
+        return (
+            <div>
+                <h2>{place.name}</h2>
+                <img src = {place.pic} alt = {place.name}></img>
+            </div>
+        )
+    })
     return (
         <Def>
             <main>
-                <h1>Places Index</h1>
+                <h1>Places Index Page</h1>
+                {placesFormatted}
             </main>
         </Def>
     )
