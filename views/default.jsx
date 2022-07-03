1fdee24dd615a5938bf5c05c8fb9c96ad78508ab
tnfo222 <github.com/tnfo222/psychic-disco>
2022-07-03 14:28:06 -0400

    Added button

--- views/default.jsx
+++ views/default.jsx
@@ -6,6 +6,7 @@ function Def (html){
         <html>
             <head>
                 <title>Title</title>
+                <link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"/>
             </head>
             <body>
                 {html.children}
--- views/home.jsx
+++ views/home.jsx
@@ -8,6 +8,9 @@ function home () {
             <main>
                 <h1>HOME</h1>
             </main>
+            <a href="/places">
+                <button className="btn-primary">Places Page</button>
+            </a>
         </Def>
     )
 }
