ba9c1042b7a383a58adac07f8e2b4414bec594e0
tnfo222 <github.com/tnfo222/psychic-disco>
2022-07-04 21:26:19 -0400

    Adding styles to REST-Rant

--- views/default.jsx
+++ views/default.jsx
@@ -1,3 +1,4 @@
+//Layout Page
 const React = require('react')
 
 //stub function
@@ -7,6 +8,7 @@ function Def (html){
             <head>
                 <title>Title</title>
                 <link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"/>
+                <link rel = "stylesheet" href = "/css/style.css"/>
             </head>
             <body>
                 {html.children}
