a3c1f0a7c8b0d478a9072102cad66906f01540c0
tnfo222 <github.com/tnfo222/psychic-disco>
2022-07-05 20:14:03 -0400

    Jazzed up form page

--- views/default.jsx
+++ views/default.jsx
@@ -7,8 +7,8 @@ function Def (html){
         <html>
             <head>
                 <title>Title</title>
-                <link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"/>
-                <link rel = "stylesheet" href = "/css/style.css"/>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous" />
+                <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
                 {html.children}
