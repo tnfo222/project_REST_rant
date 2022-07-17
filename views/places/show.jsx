3ab9071c7f64e3d7c1da52c72a91a548c05cdfb9
tnfo222 <tayforbes92@hotmail.com>
2022-07-17 13:28:26 -0400

    added edit and delete buttons

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,6 +7,7 @@ function show (data) {
           <main>
             <h1>{ data.place.name }</h1>
             <h2>Rating</h2>
+            <p>Currently unrated</p>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
               Edit
             </a>
@@ -16,6 +17,7 @@ function show (data) {
               </button>
             </form>
             <h2>Comments</h2> 
+            <p>No comments yet!</p>
           </main>
         </Def>
     )
