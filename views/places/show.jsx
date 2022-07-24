3abbf3478c309a1910ec6dcab05bd3022ed364a7
tnfo222 <tayforbes92@hotmail.com>
2022-07-24 19:30:20 -0400

    Implementation of the helper method showEstablished

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,15 +8,17 @@ function show (data) {
         <div className="row">
           <div className="col-sm-6">
             <img src={data.place.pic} alt={data.place.name} width="400" height="600"/>
+            <h3>Located in {data.place.city}, {data.place.state}</h3>
           </div>
           <div className="col-sm-6">
             <h1>{ data.place.name }</h1>
-            <h2>Location</h2>
-            <p>{data.place.city}, {data.place.state}</p>
             <h2>Rating</h2>
             <p className="inactive">Currently unrated</p>
+            <br />
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
             <h2>Cuisines</h2>
-            <p>Serving {data.place.cuisines}</p>
+            <h4>Serving {data.place.cuisines}</h4>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
               Edit
             </a>
