7fae33e6fcb16c635bb40c654d5554df8f740450
tnfo222 <tayforbes92@hotmail.com>
2022-07-24 19:37:50 -0400

    Added Validation Steps

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -79,16 +79,21 @@ function new_form () {
                             </datalist>
                         </div>
                     </div>
-                    <br></br>
+                    <br />
                     <div className="form-group">
                         <label htmlFor="cuisines">Cuisines</label>
                         <input className="form-control" id="cuisines" name="cuisines" required/>
                     </div>
-                    <div className="form-group">
+                    <div className="form-group col-sm-4">
                         <label htmlFor="founded">Founded Year</label>
-                        <input className="form-control" id="founded" name="founded" />
+                        <input 
+                            type="number"
+                            className="form-control" 
+                            id="founded" 
+                            name="founded"
+                            value={new Date().getFullYear()} />
                     </div>
-                    <br></br>
+                    <br />
                     <input className = "btn btn-primary" type="submit" value="Add Place" />
                 </form>
             </main>
