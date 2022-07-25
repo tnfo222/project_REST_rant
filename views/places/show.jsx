d5f7476d243f2ce98d27b63ff536b60e7e952cd7
tnfo222 <tayforbes92@hotmail.com>
2022-07-25 19:52:09 -0400

    Added rating in stars to the webpage

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,21 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated.
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = sumRatings / data.place.comments.length
+    rating=(
+      <h3>
+        {Math.round(averageRating)} stars
+      </h3>
+    )
     comments = data.place.comments.map( c => {
       return (
         <div className="border col-sm-3">
@@ -35,7 +49,7 @@ function show (data) {
           <div className="col-sm-6">
             <h1>{ data.place.name }</h1>
             <h2>Rating</h2>
-            <p className="inactive">No rating yet</p>
+            {rating}
             <br />
             <h2>Description</h2>
             <h3>{data.place.showEstablished()}</h3>
