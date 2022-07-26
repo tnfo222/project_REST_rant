5908e6d1fda1fdd93ca9d192516f82381540f81b
tnfo222 <tayforbes92@hotmail.com>
2022-07-25 21:29:59 -0400

    fixed index

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-//Modules and Globals
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
-//Decrypt data
-app.use(express.urlencoded({ extended: true }))
-//Add PUT and DELETE methods
-app.use(methodOverride('_method'))
 
-//Express Settings
-app.set('views', __dirname + '/views')
+// Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
-//Controllers and Routes
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
-//Home Page Route
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-//Error 404 Page Route
 app.get('*', (req, res) => {
     res.render('error404')
 })
 
-//Listen for Connections
+// Listen for Connections
 app.listen(process.env.PORT)
